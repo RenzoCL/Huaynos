99bf75a41e9f668b56e04feaaefa827f170b44f6
RenzoCL <77072225+RenzoCL@users.noreply.github.com>
2026-02-20 19:01:28 -0500

    Initialize Firebase app and authentication

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBu1KKK7YJsXoHbNNUXLBhV54b1UIS8OEw",
+  authDomain: "huaynos-6e527.firebaseapp.com",
+  projectId: "huaynos-6e527",
+  storageBucket: "huaynos-6e527.firebasestorage.app",
+  messagingSenderId: "374495999469",
+  appId: "1:374495999469:web:54dc33730a53f33e2a63e4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
